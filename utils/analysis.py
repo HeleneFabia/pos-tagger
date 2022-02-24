cf4ee5a9286180a7e5aafcc094616574e071ed9d
Helene Kortschak <helene.kortschak@gmail.com>
2022-02-24 16:47:23 +0100

    Add files via upload

--- utils/analysis.py
+++ utils/analysis.py
@@ -4,6 +4,55 @@ import pandas as pd
 import torch
 
 
+pos2description = {
+    "CC": "Coordinating conjunction",
+    "CD": "Cardinal number",
+    "DT": "Determiner",
+    "EX": "Existential there",
+    "FW": "Foreign word",
+    "IN": "Preposition or subordinating conjunction",
+    "JJ": "Adjective",
+    "JJR": "Adjective, comparative",
+    "JJS": "Adjective, superlative",
+    "LS": "List item marker",
+    "MD": "Modal",
+    "NN": "Noun, singular or mass",
+    "NNS": "Noun, plural",
+    "NNP": "Proper noun, singular",
+    "NNPS": "Proper noun, plural",
+    "PDT": "Predeterminer",
+    "POS": "Possessive ending",
+    "PRP": "Personal pronoun",
+    "PRP$": "Possessive pronoun",
+    "RB": "Adverb",
+    "RBR": "Adverb, comparative",
+    "RBS": "Adverb, superlative",
+    "RP": "Particle",
+    "SYM": "Symbol",
+    "TO": "to",
+    "UH": "Interjection",
+    "VB": "Verb, base form",
+    "VBD": "Verb, past tense",
+    "VBG": "Verb, gerund or present participle",
+    "VBN": "Verb, past participle",
+    "VBP": "Verb, non-3rd person singular present",
+    "VBZ": "Verb, 3rd person singular present",
+    "WDT": "Wh-determiner",
+    "WP": "Wh-pronoun",
+    "WP$": "Possessive wh-pronoun",
+    "WRB": "Wh-adverb",
+    ".": ".",
+    ",": ",",
+    ":": ":",
+    "(": "(",
+    ")": ")",
+    "#": "#",
+    "$": "$",
+    "``": "``",
+    "''": "''"
+}
+
+
 def plot_learning_curve(train_losses, valid_losses):
     plt.figure(figsize=(7, 4))
     plt.plot(range(len(train_losses)), train_losses, label="train")
@@ -68,7 +117,9 @@ def get_analysis_dataframe(
         tags
     ), reverse=True))
     # create dataframe
+    description = [pos2description[tag] for tag in tags]
     metrics = pd.DataFrame(data={
+        "description": description,
         "tag": tags,
         "test_score": test_metrics,
         "valid_score": valid_metrics,
